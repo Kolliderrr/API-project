8f8733e6d1be6d369c7c91d090fb839236b1e7ff
Kolliderrr <virus6409@gmail.com>
2024-01-26 15:49:05 +0500

    Added new path, separated models for validation

--- test_api.py
+++ test_api.py
@@ -37,3 +37,38 @@ class TestYourApp(unittest.TestCase):
         basic = requests.auth.HTTPBasicAuth(client_username, client_password)
         response = self.client.post("/query/", json={"warehouse": "example"}, auth=basic)
         self.assertEqual(response.status_code, 200)
+
+
+    def test_invalid_order(self):
+        import os
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+        client_username = os.getenv('API_CLIENT_USERNAME')
+        client_password = os.getenv('API_CLIENT_PASSWORD')
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
+        response = self.client.post("/order/", json={"warehouse": None}, auth=basic)
+        self.assertEqual(response.status_code, 422)
+
+
+    def test_valid_order(self):
+        import os
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+        client_username = os.getenv('API_CLIENT_USERNAME')
+        client_password = os.getenv('API_CLIENT_PASSWORD')
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
+        response = self.client.post("/order/", json={
+            'INN': '7721844807',
+            'warehouse': 'Тюмень',
+            'products': [
+                {
+                    'articul': '88-01401-SX',
+                    'manufacturer': 'STELLOX',
+                    'quantity': 2
+                }]
+        }, auth=basic)
+        self.assertEqual(response.status_code, 200)
