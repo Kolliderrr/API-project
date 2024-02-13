78ffd0145343fbf121ea97f12a4cfc32ecf1d4b8
Kolliderrr <virus6409@gmail.com>
2024-02-13 15:58:51 +0500

    Removed invalit tests

--- test_api.py
+++ test_api.py
@@ -1,7 +1,14 @@
 import unittest
 from fastapi.testclient import TestClient
 from inserver import app  # Замените на фактический путь к вашему приложению
-from unittest.mock import patch
+import requests
+
+def load_client_credentials(client_name):
+    import json
+    with open('clients_config.json') as f:
+        config = json.load(f)
+        return config.get(client_name, None)
+
 
 class TestYourApp(unittest.TestCase):
 
@@ -11,26 +18,41 @@ class TestYourApp(unittest.TestCase):
     def test_unauthorized(self):
         response = self.client.post("/query/", json={"warehouse": "example"})
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json(), {"detail": 'Ошибка авторизации! Введите правильные логин/пароль или обратитесь к администратору.'})  # Уточните ожидаемый результат
+        self.assertEqual(response.json(), {"detail": 'Not authenticated'})  # Уточните ожидаемый результат
 
-    @patch('inserver.load_client_credentials', return_value={"username": "test_user", "password": "test_pass"})
-    def test_invalid_query(self, mock_load_client_credentials):
-        response = self.client.post("/query/", json={})
+    def test_invalid_query(self):
+
+        client_cred = load_client_credentials('client1')
+        client_username = client_cred['username']
+        client_password = client_cred['password']
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
+        response = self.client.post("/query/", json={}, auth=basic)
         self.assertEqual(response.status_code, 422)  # Или другой код ошибки
         # Дополнительные проверки для обработки ошибок
 
-    @patch('inserver.load_client_credentials', return_value={"username": "test_user", "password": "test_pass"})
-    def test_valid_query(self, mock_load_client_credentials):
-        response = self.client.post("/query/", json={"warehouse": "example"})
+    def test_valid_query(self):
+        client_cred = load_client_credentials('client1')
+        client_username = client_cred['username']
+        client_password = client_cred['password']
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
+        response = self.client.post("/query/", json={"warehouse": "example"}, auth=basic)
         self.assertEqual(response.status_code, 200)
 
-    @patch('inserver.load_client_credentials', return_value={"username": "test_user", "password": "test_pass"})
-    def test_invalid_order(self, mock_load_client_credentials):
-        response = self.client.post("/order/", json={"warehouse": None})
+
+    def test_invalid_order(self):
+        client_cred = load_client_credentials('client1')
+        client_username = client_cred['username']
+        client_password = client_cred['password']
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
+        response = self.client.post("/order/", json={"warehouse": None}, auth=basic)
         self.assertEqual(response.status_code, 422)
 
-    @patch('inserver.load_client_credentials', return_value={"username": "test_user", "password": "test_pass"})
-    def test_valid_order(self, mock_load_client_credentials):
+
+    def test_valid_order(self):
+        client_cred = load_client_credentials('client1')
+        client_username = client_cred['username']
+        client_password = client_cred['password']
+        basic = requests.auth.HTTPBasicAuth(client_username, client_password)
         response = self.client.post("/order/", json={
             'INN': '7721844807',
             'warehouse': 'Тюмень',
@@ -40,5 +62,5 @@ class TestYourApp(unittest.TestCase):
                     'manufacturer': 'STELLOX',
                     'quantity': 2
                 }]
-        })
+        }, auth=basic)
         self.assertEqual(response.status_code, 200)
